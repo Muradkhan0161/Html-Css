26fa8fffb538d9a7d3daea4cde3009a47ceba5bc
Murad Khan <murad.khan@northsouth.edu>
2019-09-29 21:08:02 +0600

    Add files via upload

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,89 +1,135 @@
 $(document).ready(function() {
     
 
-    /* For the sticky navigation */
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == "down"){
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    
-    }, {
-        offset: '60px;'
-      });
-
-      /* Scroll on buttons */
-      $('.js--scroll-to-plans').click(function () {
-          $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1100);
-      });
-
-          /* Scroll to start */
-    $('.js--scroll-to-start').click(function () {
-        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 800); 
-     }); 
-
-     /* Navigation scroll */
-     $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+  /* For the sticky navigation */
+  $('.js--section-features').waypoint(function(direction) {
+      if (direction == "down"){
+          $('nav').addClass('sticky');
+      } else {
+          $('nav').removeClass('sticky');
       }
-    }
-  });
-
-  /* Animate on scroll */
-  $('.js--wp-1').waypoint(function(direction){
-      $('.js--wp-1').addClass('animated fadeIn');
+  
   }, {
-      offset: '50%'
-  });
+      offset: '60px;'
+    });
 
-  $('.js--wp-2').waypoint(function(direction){
-    $('.js--wp-2').addClass('animated fadeInUp');
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1100);
+    });
+
+        /* Scroll to start */
+  $('.js--scroll-to-start').click(function () {
+      $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 800); 
+   }); 
+
+   /* Navigation scroll */
+   $('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
+/* Animate on scroll */
+$('.js--wp-1').waypoint(function(direction){
+    $('.js--wp-1').addClass('animated fadeIn');
 }, {
     offset: '50%'
+});
+
+$('.js--wp-2').waypoint(function(direction){
+  $('.js--wp-2').addClass('animated fadeInUp');
+}, {
+  offset: '50%'
 });
 
 $('.js--wp-3').waypoint(function(direction){
-    $('.js--wp-3').addClass('animated fadeIn');
+  $('.js--wp-3').addClass('animated fadeIn');
 }, {
-    offset: '50%'
+  offset: '50%'
 });
 
 $('.js--wp-4').waypoint(function(direction){
-    $('.js--wp-4').addClass('animated pulse');
+  $('.js--wp-4').addClass('animated pulse');
 }, {
-    offset: '50%'
+  offset: '50%'
 });
 
+/* Mobile nav */
+
+$('.js--nav-icon').click(function() {
+  var nav = $('.js--main-nav');
+  var icon = $('.js--nav-icon i');
+  
+  nav.slideToggle(200);
+  
+  if (icon.hasClass('ion-md-menu')) {
+      icon.addClass('ion-md-close-circle-outline');
+      icon.removeClass('icon ion-md-menu');
+  } else {
+      icon.addClass('ion-md-menu');
+      icon.removeClass('ion-md-close-circle-outline');
+  }        
 });
 
+
+});
+  
+
+
+
+
+
+
+/*
+
+
+
+$('.js--nav-icon').click(function() {
+  var nav = $('.js--main-nav');
+  var icon = $('js--nav-icon menu');
+  
+  nav.slideToggle(200);
+  if (icon.hasClass('menu')) {
+  icon.addClass('close-circle-outline');
+  icon.removeClass('menu');
+  } else {
+  icon.addClass('menu');
+  icon.removeClass('close-circle-outline');
+  
+  }
+  
+  });
+
+
+  */
